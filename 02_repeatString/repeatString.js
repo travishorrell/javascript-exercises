862a9d136751841197863ae510cf1b3ccd382501
travishorrell <17819559+travishorrell@users.noreply.github.com>
2023-08-10 06:32:01 -0600

    Finished the tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
+const repeatString = function(userInput, numberOfTimes) {
+    let returnOutput = '';
 
+    if(numberOfTimes < 0)
+    {
+        returnOutput = "ERROR";
+    }
+    else
+    {
+        for(i = 0; i < numberOfTimes; i++)
+        {
+            returnOutput += userInput;
+        }
+    }
+    return returnOutput;
 };
+
+let randomNumber = Math.floor(Math.random() * 1000);
+
+repeatString('hey', randomNumber);
 
 // Do not edit below this line
 module.exports = repeatString;
